ade78ff20db63d8b23dd21b282d2a0dabf1fa2d1
zak-off-zak <Zakhar_2004@inbox.ru>
2024-10-22 21:34:40 +0200

    working output to a file

--- assembly-compiler/src/lib.rs
+++ assembly-compiler/src/lib.rs
@@ -1,13 +1,15 @@
 use clap::Parser;
 use core::fmt;
 use std::error::Error;
+use std::fs::File;
+use std::io::Write;
 use std::{fs, u8};
 
 #[derive(Parser)]
 pub struct Config {
     #[arg(short, long)]
     input_file_path: String,
-    #[arg(short, long, default_value_t = String::from("a.txt"))]
+    #[arg(short, long, default_value_t = String::from("a.out"))]
     output_file_path: String,
 }
 
@@ -51,7 +53,7 @@ fn get_opcode_number_from_str(opcode: &str) -> Result<String, &'static str> {
     }
 }
 
-pub fn compile(contents: &str) -> Result<(), Box<dyn Error>> {
+pub fn compile(contents: &str, output_file_path: &str) -> Result<(), Box<dyn Error>> {
     let mut cnt: u8 = 0;
     let mut instructions: Vec<Instruction> = Vec::new();
     for line in contents.lines() {
@@ -62,20 +64,29 @@ pub fn compile(contents: &str) -> Result<(), Box<dyn Error>> {
         instructions.push(Instruction::new(count, &opcode, argument));
         cnt += 1;
     }
+    for i in cnt..16 {
+        instructions.push(Instruction::new(i, "0000", "00000"));
+    }
+    println!("Resulting instructions: ");
+    let mut output = File::create(output_file_path)?;
     for instruction in instructions {
         println!("{instruction}");
+        writeln!(output, "{instruction}")?;
     }
     Ok(())
 }
 
 pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
-    println!("Compiling the assembly form {}", config.input_file_path);
+    println!(
+        "Compiling the assembly form {} into {}",
+        config.input_file_path, config.output_file_path
+    );
 
     let contents = fs::read_to_string(config.input_file_path)?;
 
-    println!("Contents of the file:\n{contents}");
+    println!("Contents of the input file:\n{contents}");
 
-    let _ = compile(&contents);
+    let _ = compile(&contents, &config.output_file_path);
 
     Ok(())
 }
