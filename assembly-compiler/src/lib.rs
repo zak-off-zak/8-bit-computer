ac14a8f6cd403bc4fb03af6ea0e9183661d4e96d
zak-off-zak <Zakhar_2004@inbox.ru>
2024-10-22 23:37:44 +0200

    PRAM module added and a new compilation mode implemeted

--- assembly-compiler/src/lib.rs
+++ assembly-compiler/src/lib.rs
@@ -11,20 +11,22 @@ pub struct Config {
     input_file_path: String,
     #[arg(short, long, default_value_t = String::from("a.out"))]
     output_file_path: String,
+    #[clap(long, short, default_value_t = false)]
+    logisim_ram: bool,
 }
 
 #[derive(Debug)]
 pub struct Instruction {
     count: u8,
-    opcode: String,
+    opcode: u8,
     argument: String,
 }
 
 impl Instruction {
-    fn new(count: u8, opcode: &str, argument: &str) -> Instruction {
+    fn new(count: u8, opcode: u8, argument: &str) -> Instruction {
         Instruction {
             count,
-            opcode: opcode.to_owned(),
+            opcode,
             argument: argument.to_owned(),
         }
     }
@@ -32,28 +34,36 @@ impl Instruction {
 
 impl fmt::Display for Instruction {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        write!(f, "{:04b}: {} {}", self.count, self.opcode, self.argument)
+        write!(
+            f,
+            "{:04b}: {:04b} {}",
+            self.count, self.opcode, self.argument
+        )
     }
 }
 
-fn get_opcode_number_from_str(opcode: &str) -> Result<String, &'static str> {
+fn get_opcode_number_from_str(opcode: &str) -> Result<u8, &'static str> {
     match opcode {
-        "nop" => Ok(String::from("0000")),
-        "lda" => Ok(String::from("0001")),
-        "add" => Ok(String::from("0010")),
-        "sub" => Ok(String::from("0011")),
-        "sta" => Ok(String::from("0100")),
-        "ldi" => Ok(String::from("0101")),
-        "jmp" => Ok(String::from("0110")),
-        "jc" => Ok(String::from("0111")),
-        "jz" => Ok(String::from("1000")),
-        "out" => Ok(String::from("1110")),
-        "hlt" => Ok(String::from("1111")),
+        "nop" => Ok(0),
+        "lda" => Ok(1),
+        "add" => Ok(2),
+        "sub" => Ok(3),
+        "sta" => Ok(4),
+        "ldi" => Ok(5),
+        "jmp" => Ok(6),
+        "jc" => Ok(7),
+        "jz" => Ok(8),
+        "out" => Ok(14),
+        "hlt" => Ok(15),
         _ => Err("Wrong opcode: {_}!"),
     }
 }
 
-pub fn compile(contents: &str, output_file_path: &str) -> Result<(), Box<dyn Error>> {
+pub fn compile(
+    contents: &str,
+    output_file_path: &str,
+    logisim_ram: &bool,
+) -> Result<(), Box<dyn Error>> {
     let mut cnt: u8 = 0;
     let mut instructions: Vec<Instruction> = Vec::new();
     for line in contents.lines() {
@@ -61,17 +71,30 @@ pub fn compile(contents: &str, output_file_path: &str) -> Result<(), Box<dyn Err
         let line_contents: Vec<&str> = line.split_whitespace().collect();
         let opcode = get_opcode_number_from_str(&line_contents[0].to_lowercase())?;
         let argument = line_contents[1];
-        instructions.push(Instruction::new(count, &opcode, argument));
+        instructions.push(Instruction::new(count, opcode, argument));
         cnt += 1;
     }
     for i in cnt..16 {
-        instructions.push(Instruction::new(i, "0000", "0000"));
+        instructions.push(Instruction::new(i, 0, "0"));
     }
     println!("Resulting instructions: ");
     let mut output = File::create(output_file_path)?;
-    for instruction in instructions {
-        println!("{instruction}");
-        writeln!(output, "{instruction}")?;
+    if !logisim_ram {
+        for instruction in instructions {
+            println!("{instruction}");
+            writeln!(output, "{instruction}")?;
+        }
+    } else {
+        print!("v3.0 hex words addressed\n0:");
+        write!(output, "v3.0 hex words addressed\n0:");
+        for instruction in instructions {
+            print!(" {:1x}{} ", instruction.opcode, instruction.argument);
+            write!(
+                output,
+                " {:1x}{} ",
+                instruction.opcode, instruction.argument
+            )?;
+        }
     }
     Ok(())
 }
@@ -86,7 +109,7 @@ pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
 
     println!("Contents of the input file:\n{contents}");
 
-    let _ = compile(&contents, &config.output_file_path);
+    let _ = compile(&contents, &config.output_file_path, &config.logisim_ram);
 
     Ok(())
 }
